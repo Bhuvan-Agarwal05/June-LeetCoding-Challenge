16cc805ee0073148ba638ef28b51570fed17ba7c
Bhuvan-Agarwal <bhuvangarg123@gmail.com>
2020-06-04 13:43:14 +0530

    Reverse String Added

---
+++ Day 4 (4th June,2020)/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low=0;
+        int high=s.length-1;
+        while(low<high)
+            {
+            char ch = s[high];
+            s[high]=s[low];
+            s[low] = ch;
+            low++;
+            high--;
+            }
+
+    }
+}
